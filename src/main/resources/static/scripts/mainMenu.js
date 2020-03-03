53206fcf7f0045d6f5541e964299db47ffaaea5c
Chris Troupe <christophertroupe@gmail.com>
2020-03-03 15:10:45 -0600

    changed the JS file from mainMenu to master

--- src/main/resources/static/scripts/mainMenu.js
+++ src/main/resources/static/scripts/mainMenu.js
@@ -6,17 +6,6 @@ document.getElementById("createEmployee").addEventListener("click", navigateEmpl
 document.getElementById("salesReport").addEventListener("click", displayError);
 document.getElementById("cashiersReport").addEventListener("click", displayError);
 
-
-// eventually should move to master.js
-document.getElementById("signOut").addEventListener("click", makeRequest);
-function makeRequest() {
-  var xhttp = new XMLHttpRequest();
-  // use ajaxDelete() but don't know how to call it at the moment
-
-  location.replace("https://www.w3schools.com") // Navigate to the sign in page
-}
-
-
 // need to navigate to the productListing view
 // navigate to new employee detail view
 function navigateEmployeeDetail() {
@@ -24,7 +13,7 @@ location.assign("https://www.w3schools.com"); // should go to employee detail we
 }
 
 function navigateProductListing() {
-location.assign("https://www.w3schools.com"); // should go to product listing webpage
+location.assign("https://misfits5-app.herokuapp.com"); // should go to product listing webpage
 }
 
 function displayError() {
